7ac49b9bbfc00f119f4c10effb940eeaa07d0479
milliwonkim <rldnjs9347@gmail.com>
2020-02-18 17:38:02 +0900

    Updates

---
+++ precache-manifest.5a3bcc89f11740649329f29dd2b486e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78baa7946f4d372d1059256ba64fb8d2",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "2bc5c0c8fda08e77337d",
+    "url": "/recipe/static/css/main.c0a64ec2.chunk.css"
+  },
+  {
+    "revision": "59486b68e1df1893dd35",
+    "url": "/recipe/static/js/2.d3f3fe40.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipe/static/js/2.d3f3fe40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc5c0c8fda08e77337d",
+    "url": "/recipe/static/js/main.3d1e22c3.chunk.js"
+  },
+  {
+    "revision": "70b0b55a4cc8a4f64c71",
+    "url": "/recipe/static/js/runtime-main.b2c80479.js"
+  }
+]);
